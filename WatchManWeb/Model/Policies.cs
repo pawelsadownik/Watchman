d855aef921c350260c43ba5b6d731902bd590920
sadownikp <p.sadownik@npc.pl>
2020-04-18 13:13:55 +0200

    authoriztion models

---
+++ WatchManWeb/Model/Policies.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WatchmanWeb.Model
+{
+    public class Policies
+    {
+        public const string Admin = "Admin";
+        public const string AdvancedUser = "AdvancedUser";
+        public const string BasicUser = "BasicUser";
+
+        public static AuthorizationPolicy AdminPolicy()
+        {
+            return new AuthorizationPolicyBuilder().RequireAuthenticatedUser().RequireRole(Admin).Build();
+        }
+
+        public static AuthorizationPolicy AdvancedUserPolicy()
+        {
+            return new AuthorizationPolicyBuilder().RequireAuthenticatedUser().RequireRole(AdvancedUser).Build();
+        }
+
+        public static AuthorizationPolicy BasicUserPolicy()
+        {
+            return new AuthorizationPolicyBuilder().RequireAuthenticatedUser().RequireRole(BasicUser).Build();
+        }
+    }
+}
