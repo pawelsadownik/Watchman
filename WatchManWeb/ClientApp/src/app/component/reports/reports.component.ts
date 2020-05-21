8da938b6bdcc6d745ea857d99d5437df0e7a4165
sadownikp <p.sadownik@npc.pl>
2020-05-22 01:21:03 +0200

    charts

--- WatchManWeb/ClientApp/src/app/component/reports/reports.component.ts
+++ WatchManWeb/ClientApp/src/app/component/reports/reports.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { BaseComponent } from '../base/base.component';
 import { Incident } from '../../model/incident';
 import { IncidentService } from '../../service/incident.service';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
 
 @Component({
   selector: 'app-reports',
@@ -12,15 +14,18 @@ export class ReportsComponent extends BaseComponent implements OnInit {
 
   incidents: Incident[] = [];
   cols: any[];
+  private chart: am4charts.XYChart;
 
   constructor(
-    public incidentService: IncidentService) { super(); }
+    public incidentService: IncidentService,
+    private zone: NgZone) { super(); }
 
   ngOnInit() {
     this.safeSub(
       this.incidentService.getAll().subscribe(
         (incidents) => {
           this.incidents = incidents;
+          this.generateChart();
         }
       )
     );
@@ -32,7 +37,76 @@ export class ReportsComponent extends BaseComponent implements OnInit {
     ];
   }
 
-  generateChart() { }
+  generateChart() {
+    this.zone.runOutsideAngular(() => {
+      let chart = am4core.create('chartdiv', am4charts.XYChart)
+
+      chart.legend = new am4charts.Legend()
+      chart.legend.position = 'top'
+      chart.legend.paddingBottom = 20
+      chart.legend.labels.template.maxWidth = 95
+
+      let xAxis = chart.xAxes.push(new am4charts.CategoryAxis())
+      xAxis.dataFields.category = 'category'
+      xAxis.renderer.cellStartLocation = 0.1
+      xAxis.renderer.cellEndLocation = 0.9
+      xAxis.renderer.grid.template.location = 0;
+
+      let yAxis = chart.yAxes.push(new am4charts.ValueAxis());
+      yAxis.min = 0;
+
+      function createSeries(value, name) {
+        let series = chart.series.push(new am4charts.ColumnSeries())
+        series.dataFields.valueY = value
+        series.dataFields.categoryX = 'category'
+        series.name = name
+        if (series.name == 'RED') {
+          series.columns.template.stroke = am4core.color("#bf231b"); // red outline
+          series.columns.template.fill = am4core.color("#bf231b");
+        }
+        if (series.name == 'YELLOW') {
+          series.columns.template.stroke = am4core.color("#ebe700"); // red outline
+          series.columns.template.fill = am4core.color("#ebe700");        }
+
+        //series.events.on("hidden", arrangeColumns);
+        //series.events.on("shown", arrangeColumns);
+
+        let bullet = series.bullets.push(new am4charts.LabelBullet())
+        bullet.interactionsEnabled = false
+        bullet.dy = 30;
+        bullet.label.text = '{valueY}'
+        bullet.label.fill = am4core.color('#ffffff')
+        
+        return series;
+      }
+
+      chart.data = [
+        {
+          category: 'Cam #1',
+          red: 40,
+          yellow: 55
+        },
+        {
+          category: 'Cam #2',
+          red: 30,
+          yellow: 78
+        },
+        {
+          category: 'Cam #3',
+          red: 27,
+          yellow: 40
+        },
+        {
+          category: 'Cam #4',
+          red: 50,
+          yellow: 33
+        }
+      ]
+
+      createSeries('red', 'RED');
+      createSeries('yellow', 'YELLOW');
+    });
+  }
 
 
   generatePdf() {
@@ -42,5 +116,9 @@ export class ReportsComponent extends BaseComponent implements OnInit {
         err => {
           console.log(err);
         })
+  }
+
+  countIncidents() {
+
   }
 }
